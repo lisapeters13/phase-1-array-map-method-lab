a85176068c540aa3402888b2ff9f3253b15ff256
lisapeters13 <lisapetersirl@gmail.com>
2024-06-20 17:23:53 -0400

    Initial commit

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const titleCasedArray = tutorials.map(name => {
+    const words = name.split(' ');
+    const capitalizedWords = words.map(word => {
+      const capitalizedWord = word[0].toUpperCase() + word.slice(1);
+      return capitalizedWord;
+    });
+    return capitalizedWords.join(' ');
+  });
+
+  return titleCasedArray;
 }
+
+const expected = [
+  'What Does The This Keyword Mean?',
+  'What Is The Constructor OO Pattern?',
+  'Implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What Is NaN And How Can We Check For It',
+  'What Is The Difference Between StopPropagation And PreventDefault?',
+  'Immutable State And Pure Functions',
+  'What Is The Difference Between == And ===?',
+  'What Is The Difference Between Event Capturing And Bubbling?',
+  'What Is JSONP?'
+];
+
+console.log(titleCased());
